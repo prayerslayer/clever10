b93145b83731e47a3068fa6b0bbdeb162b092382
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2022-07-16 14:49:11 +0200

    allow card categories via context

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
 import random
 
+def rgb_hex_to_int(hex_code):
+    red = int(hex_code[0:2], 16)
+    green = int(hex_code[2:4], 16)
+    blue = int(hex_code[4:6], 16)
+    return [red, green, blue]
 
 def content_type_to_dict(option_or_answer):
     if not option_or_answer.startswith("["):
@@ -10,14 +15,12 @@ def content_type_to_dict(option_or_answer):
         return {"type": "image", "content": option_or_answer[5:-1]}
     elif option_or_answer.startswith("[color:"):
         hex_code = option_or_answer[7:-1]
-        red = int(hex_code[0:2], 16)
-        green = int(hex_code[2:4], 16)
-        blue = int(hex_code[4:6], 16)
-        return {"type": "color", "content": [red, green, blue]}
+        rgb = rgb_hex_to_int(hex_code)
+        return {"type": "color", "content": rgb}
 
     raise Exception("Unknown content type")
 
-def parse_question(question_str, seed, debug):
+def parse_question(question_str, ctx, seed, debug):
     question = question_str[0]
     options = list(
         map(lambda x: content_type_to_dict(x.split(";")[1]), question_str[1:])
@@ -30,13 +33,22 @@ def parse_question(question_str, seed, debug):
 
     options = [options[i] for i in ordering]
     answers = [answers[i] for i in ordering]
+    return {"question": question, "options": options, "answers": answers, 'ctx': ctx}
 
-    return {"question": question, "options": options, "answers": answers}
+def parse_context(line):
+    content = line[1:-1] # remove brackets
+    tuples = content.split(',')
+    ctx = {}
+    for t in tuples:
+        [key, val] = t.split(':')
+        ctx[key] = val
+    return ctx
 
 
 def read_questions(path_to_file, seed, debug):
     questions = list()
     random.seed(seed)
+    context = {'color':'black'}
     with open(path_to_file, "r") as f:
         while True:
             cntr = 11
@@ -45,12 +57,16 @@ def read_questions(path_to_file, seed, debug):
                 line = f.readline()
                 if line == "\n" or line.startswith("#"):
                     continue
+                if cntr == 11 and line.startswith('{'):
+                    context = parse_context(line.strip())
+                    continue
+                #EOF
                 if line == "":
                     return questions
 
                 question_str.append(line.strip())
                 cntr -= 1
-            questions.append(parse_question(question_str, seed, debug))
+            questions.append(parse_question(question_str, context, seed, debug))
     if not debug or seed > 0:
         random.shuffle(questions)
     return questions
